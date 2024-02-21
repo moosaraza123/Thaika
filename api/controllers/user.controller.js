29e8f65ac76c48fffd6ccfb6d0db467bbf354799
moosa <razamoosa538@gmail.com>
2024-02-21 13:45:06 +0500

    create get user api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
+import Listing from '../models/listing.model.js';
 export const test = (req, res) => {
     res.json({
       message: 'API is working!',
@@ -45,3 +46,16 @@ export const test = (req, res) => {
       next(error);
     }
   };
+  export const getUserListings = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+      try {
+        const listings = await Listing.find({ userRef: req.params.id });
+        res.status(200).json(listings);
+      } catch (error) {
+        next(error);
+      }
+    } else {
+      return next(errorHandler(401, 'You can only view your own listings!'));
+    }
+  };
+  
