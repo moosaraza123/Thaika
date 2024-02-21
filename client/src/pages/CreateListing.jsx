d30570202b47bbcb35e5dfa0f62c33271969cf3c
moosa <razamoosa538@gmail.com>
2024-02-21 13:16:08 +0500

    complete create listing page functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,32 +1,35 @@
-import React from 'react';
-import './style/CreateListing.css';
-import { useState } from 'react';
+import React from "react";
+import "./style/CreateListing.css";
+import { useState } from "react";
 import {
   getDownloadURL,
   getStorage,
   ref,
   uploadBytesResumable,
-} from 'firebase/storage';
-import { app } from '../firebase';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+} from "firebase/storage";
+import { app } from "../firebase";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function CreateListing() {
+  const { currentUser } = useSelector((state) => state.user);
   const [files, setFiles] = useState([]);
   const [formData, setFormData] = useState({
-    imageUrls: [],
-    name: '',
-    description: '',
-    
-    maxPrice: 50,
-    minPrice: 0,
-    
+    name:" ",
+    description:" ",
+ 
+    maxPrice:0,
+    minPrice:0,
+    experience:0,
+    imageUrls:[]
   });
+  const navigate=useNavigate()
+  console.log(formData);
   const [imageUploadError, setImageUploadError] = useState(false);
   const [uploading, setUploading] = useState(false);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
-  console.log(files)
+
   const handleImageSubmit = (e) => {
     if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
       setUploading(true);
@@ -46,11 +49,11 @@ function CreateListing() {
           setUploading(false);
         })
         .catch((err) => {
-          setImageUploadError('Image upload failed (2 mb max per image)');
+          setImageUploadError("Image upload failed (2 mb max per image)");
           setUploading(false);
         });
     } else {
-      setImageUploadError('You can only upload 6 images per listing');
+      setImageUploadError("You can only upload 6 images per listing");
       setUploading(false);
     }
   };
@@ -61,7 +64,7 @@ function CreateListing() {
       const storageRef = ref(storage, fileName);
       const uploadTask = uploadBytesResumable(storageRef, file);
       uploadTask.on(
-        'state_changed',
+        "state_changed",
         (snapshot) => {
           const progress =
             (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
@@ -84,57 +87,157 @@ function CreateListing() {
       imageUrls: formData.imageUrls.filter((_, i) => i !== index),
     });
   };
+  const handleChange = (e) => {
+    if (
+      e.target.type === "number" ||
+      e.target.type === "text" ||
+      e.target.type === "textarea"
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
+    }
+  };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (formData.imageUrls.length < 1)
+        return setError('You must upload at least one image');
+      if (+formData.regularPrice < +formData.discountPrice)
+        return setError('Discount price must be lower than regular price');
+      setLoading(true);
+      setError(false);
+      const res = await fetch('/api/listing/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ...formData,
+          userRef: currentUser._id,
+        }),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(data.message);
+      }
+      navigate(`/listing/${data._id}`);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
 
   return (
     <>
-      <main className='maintagDiv'>
-        <h1 id='heading'>Create a Listing</h1>
-          <form className='form'>
-          <div className='form-group'>
-            <label htmlFor='name'>Name:</label>
-            <input type='text' id='name' name='name' required />
+      <main className="maintagDiv">
+        <h1 id="heading">Create a Listing</h1>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Name of service :</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              required
+              onChange={handleChange}
+              value={formData.name}
+            />
           </div>
-          <div className='form-group'>
-            <label htmlFor='description'>Description:</label>
-            <textarea id='description' name='description' required></textarea>
+          <div className="form-group">
+            <label htmlFor="description">Description of service :</label>
+            <textarea
+              id="description"
+              name="description"
+              required
+              onChange={handleChange}
+              value={formData.description}
+            ></textarea>
           </div>
-          <div className='form-group'>
-            <label htmlFor='experience'>Experience (years):</label>
-            <input type='number' id='experience' name='experience' required />
+
+          <div className="form-group">
+            <label htmlFor="maxPrice">Maximum Price:</label>
+            
+            <input
+                type='number'
+                id='maxPrice'
+             
+                required
+                
+                onChange={handleChange}
+                value={formData.maxPrice}
+              />
+            
+
+
           </div>
-          <div className='form-group'>
-            <label htmlFor='max-price'>Maximum Price:</label>
-            <input type='number' id='max-price' name='max-price' required />
+          <div className="form-group">
+            <label htmlFor="minPrice">Minimum Price:</label>
+            <input
+                  type='number'
+                  id='minPrice'
+                 
+                  required
+                 
+                  onChange={handleChange}
+                  value={formData.minPrice}
+                />
           </div>
-          <div className='form-group'>
-            <label htmlFor='min-price'>Minimum Price:</label>
-            <input type='number' id='min-price' name='min-price' required />
+
+
+
+
+
+
+          <div className="form-group">
+            <label htmlFor="experience">Experience (years):</label>
+            <input
+              type="number"
+              id="experience"
+              name="experience"
+              required
+              onChange={handleChange}
+              value={formData.experience}
+            />
           </div>
-          <div className='form-group'>
+         
+          <div className="form-group">
             <p>images: First image will be cover max (6) work samples</p>
-           
-            <label htmlFor='image'>Upload Image of work samples</label>
-            <input onChange={(e)=>setFiles(e.target.files)} type='file' id='image' name='image' accept='image/*' required  multiple/>
+
+            <label htmlFor="image">Upload Image of work samples</label>
+            <input
+              onChange={(e) => setFiles(e.target.files)}
+              type="file"
+              id="image"
+              name="image"
+              accept="image/*"
+              required
+              multiple
+            />
           </div>
-          <button type="button" id='uploadbtn' onClick={handleImageSubmit} >{uploading?'Uploading...':'Upload'}</button>
+          <button type="button" id="uploadbtn" onClick={handleImageSubmit}>
+            {uploading ? "Uploading..." : "Upload"}
+          </button>
           <p>{imageUploadError && imageUploadError}</p>
           {formData.imageUrls.length > 0 &&
             formData.imageUrls.map((url, index) => (
-              <div
-                key={url}
-                className='custom-container'
-              >
-                <img
-                  src={url}
-                  alt='listing image'
-                  className='custom-image'
-                />
+              <div key={url} className="custom-container">
+                <img src={url} alt="listing image" className="custom-image" />
                 <button
-                  type='button' id='uploadbtn' onClick={()=> handleRemoveImage(index)}>Delete </button>
-          </div>))}
-          <button type='submit'>Create Listing</button>
-          
+                  type="button"
+                  id="uploadbtn"
+                  onClick={() => handleRemoveImage(index)}
+                >
+                  Delete{" "}
+                </button>
+              </div>
+            ))}
+          <button type="submit"> {loading ? 'Creating...' : 'Create listing'}</button>
+
+          {error && <p style={{color: 'red'}} >{error}</p>}
         </form>
       </main>
     </>
