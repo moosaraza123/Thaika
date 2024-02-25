afc4ca50332aae39a0f0af93917e25ced7c0159b
moosa <razamoosa538@gmail.com>
2024-02-25 13:02:12 +0500

    complete search funtionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,65 +1,86 @@
 // Header.js
 // import { FaSearch } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import React from 'react';
-
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 import Logo from '/WebLogo.png';
 import home from '/home.png';
 import './Header.css';
-import {useSelector} from 'react-redux'
+
+import {
+  FaSearch
+} from 'react-icons/fa';
 
 function Header() {
+  const [searchTerm, setSearchTerm] = useState('');
   const { currentUser } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   return (
     <>
-      <div className="mainHeadDiv">
-        <ul>
-          <li>
-            <Link to='/'>
-              <img src={Logo} alt="" />
-            </Link>
-          </li>
-
-          <li>
-            <form>
-              <input type="search" />
-            </form>
-          </li>
-
-          <li>
-            <Link to='/'>
-              <img src={home} alt="" />
-            </Link>
-          </li>
-
-          <li>
-            <Link to='/about' className="no-underline">
-              About
-            </Link>
-          </li>
-
-          {/* <li>
-            
-            <Link to='/sign-in' className="no-underline">
-              Sign In
-            </Link>
-          </li> */}
-          <Link to='/profile'>
-            {currentUser ? (
-              <img
-                className='headerprofileimg'
-                src={currentUser.avatar}
-                alt='profile'
-              />
-            ) : (
-              <li className='no-underline'> Sign in</li>
-            )}
+    <div className="mainHeadDiv">
+      <ul>
+        <li>
+          <Link to='/'>
+            <img src={Logo} alt="" />
           </Link>
+        </li>
+  
+        <li>
+          <form   onSubmit={handleSubmit}>
+          <input type="search"    value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
 
+          <button className="search-button">
+  <FaSearch className="search-icon" />
+</button>
 
-        </ul>
-      </div>
-    </>
+            
+          </form>
+        </li>
+  
+        <li>
+          <Link to='/'>
+            <img src={home} alt="" />
+          </Link>
+        </li>
+  
+        <li>
+          <Link to='/about' className="no-underline">
+            About
+          </Link>
+        </li>
+  
+       <Link to='/profile'>
+  {currentUser ? (
+    <img
+      className='headerprofileimg'
+      src={currentUser.avatar}
+      alt='profile'
+    />
+  ) : (
+    <li  className='no-underline'>Sign in</li>
+  )}
+</Link>
+
+      </ul>
+    </div>
+  </>
+  
   );
 }
 
