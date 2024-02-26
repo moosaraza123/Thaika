3ebd6b7ec55d7ca2eb53777a713d0ea1d94f14fa
moosa <razamoosa538@gmail.com>
2024-02-26 14:32:45 +0500

    add pre installation

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
+import path from 'path';
+
 const app = express();
 app.use(express.json())
 app.use(cookieParser());
@@ -20,10 +22,16 @@ app.listen(3000, () => {
   .catch((err) => {
     console.log(err);
   });
-
+  const __dirname = path.resolve();
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
