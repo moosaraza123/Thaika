57b9294c14d3a7449a458fe36a7cd0a815d3507c
moosa <razamoosa538@gmail.com>
2024-06-09 14:21:43 +0500

    final up

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:3000',
+//         secure: false,
+//       },
+//     },
+//   },
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,8 +25,9 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:3000',
         secure: false,
+        changeOrigin: true,
       },
     },
   },
   plugins: [react()],
-})
+});
